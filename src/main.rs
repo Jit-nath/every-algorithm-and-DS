ec9971388a55bcf6027bade717e41c895cab2cec
Jit Debnath <jit.nathdeb@gmail.com>
2025-12-05 13:30:45 +0530

    use in main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use every_algorithm_and_ds::algorithms::sorting::quick_sort::quick_sort;
+
+
 fn main() {
-    println!("Hello, world!");
+    let mut arr: [i32; 6] = [6, 5, 4, 3, 2, 1];
+
+    quick_sort(&mut arr);
+
+    println!("{:?}", arr);
 }
